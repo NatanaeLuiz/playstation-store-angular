46be95e1269184688fb2b8149002df9c85024756
Natanael Rolim <natanael.10@hotmail.com>
2023-10-09 23:20:20 -0300

    REFACTOR/Componentes
    - Repassando parametros de componentes Pais para os filhos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  gamerCover:string = ""
+  @Input()
+  gameLabel: string=""
+  @Input()
+  gameType:string = "Digital PS4"
+  @Input()
+  gamePrice:string = "R$ 399,99"
+  
   constructor() { }
 
   ngOnInit(): void {
